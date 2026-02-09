91c7d0b6eae3dfb0f998153f1efbb79af621e6ee
ibrahimqarayev <qarayevibrahim7@mail.ru>
2026-02-09 18:28:31 +0400

    Add UserDto

---
+++ src/main/java/com/ibrahim/restaurant/dto/user/UserDto.java
@@ -0,0 +1,18 @@
+package com.ibrahim.restaurant.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+
+    private String id;
+    private String uname;
+    private String givenName;
+    private String familyName;
+}
