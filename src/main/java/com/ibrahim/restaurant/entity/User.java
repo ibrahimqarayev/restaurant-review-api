b11cd5f3e43fbb2047f4e1eec4c9856488585819
ibrahimqarayev <qarayevibrahim7@mail.ru>
2026-02-09 18:28:24 +0400

    Add User

---
+++ src/main/java/com/ibrahim/restaurant/entity/User.java
@@ -0,0 +1,27 @@
+package com.ibrahim.restaurant.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Field(type = FieldType.Keyword)
+    private String id;
+
+    @Field(type = FieldType.Text)
+    private String uname;
+
+    @Field(type = FieldType.Text)
+    private String givenName;
+
+    @Field(type = FieldType.Text)
+    private String familyName;
+}
