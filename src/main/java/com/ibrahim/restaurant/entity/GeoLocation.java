c24b1000af16e7b8898b7f82f912cf35eddca7c9
ibrahimqarayev <qarayevibrahim7@mail.ru>
2026-02-10 18:30:00 +0400

    Add GeoLocation

---
+++ src/main/java/com/ibrahim/restaurant/entity/GeoLocation.java
@@ -0,0 +1,16 @@
+package com.ibrahim.restaurant.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class GeoLocation {
+
+    private Double latitude;
+    private Double longitude;
+}
