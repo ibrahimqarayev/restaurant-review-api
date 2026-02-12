bac6ddc16d99abac2d113bb8564c9f71a7f85950
ibrahimqarayev <qarayevibrahim7@mail.ru>
2026-02-12 18:31:19 +0400

    Add RestaurantUpdateRequest

---
+++ src/main/java/com/ibrahim/restaurant/api/restaurant/RestaurantUpdateRequest.java
@@ -0,0 +1,27 @@
+package com.ibrahim.restaurant.api.restaurant;
+
+import com.ibrahim.restaurant.entity.Address;
+import com.ibrahim.restaurant.entity.OperatingHours;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RestaurantUpdateRequest {
+
+    private String name;
+    private String cuisineType;
+    private String contactInformation;
+    private Address address;
+    private OperatingHours operatingHours;
+    private List<String> photoIds;
+}
