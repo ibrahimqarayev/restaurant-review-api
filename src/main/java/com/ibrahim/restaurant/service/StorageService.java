8a38151275cdf8486298b9bcc5c83999483b73ba
ibrahimqarayev <qarayevibrahim7@mail.ru>
2026-02-14 18:32:53 +0400

    Add StorageService

---
+++ src/main/java/com/ibrahim/restaurant/service/StorageService.java
@@ -0,0 +1,12 @@
+package com.ibrahim.restaurant.service;
+
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Optional;
+
+public interface StorageService {
+    String store(MultipartFile file, String fileName);
+
+    Optional<Resource> loadAsResource(String fileName);
+}
