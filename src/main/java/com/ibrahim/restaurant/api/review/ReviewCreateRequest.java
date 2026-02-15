45adf403a8462f08eea9a32517ae6ed92168cfa5
ibrahimqarayev <qarayevibrahim7@mail.ru>
2026-02-15 18:33:28 +0400

    Add ReviewCreateRequest

---
+++ src/main/java/com/ibrahim/restaurant/api/review/ReviewCreateRequest.java
@@ -0,0 +1,21 @@
+package com.ibrahim.restaurant.api.review;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReviewCreateRequest {
+
+    private String content;
+    private Integer rating;
+    private List<String> photoIds;
+}
