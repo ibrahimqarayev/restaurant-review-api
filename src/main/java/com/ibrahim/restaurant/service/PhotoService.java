b0d527f5e1cbe66d2bd19f2651bf20563af260d7
ibrahimqarayev <qarayevibrahim7@mail.ru>
2026-02-17 18:35:06 +0400

    Add PhotoService

---
+++ src/main/java/com/ibrahim/restaurant/service/PhotoService.java
@@ -0,0 +1,13 @@
+package com.ibrahim.restaurant.service;
+
+import com.ibrahim.restaurant.entity.Photo;
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Optional;
+
+public interface PhotoService {
+    Photo uploadPhoto(MultipartFile file);
+
+    Optional<Resource> getPhotoAsResource(String id);
+}
