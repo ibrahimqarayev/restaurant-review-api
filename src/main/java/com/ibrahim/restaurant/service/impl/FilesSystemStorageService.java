2f4288a09f2082e1cc0221e21a6759d71089cc4a
ibrahimqarayev <qarayevibrahim7@mail.ru>
2026-02-18 18:36:09 +0400

    Add FilesSystemStorageService

---
+++ src/main/java/com/ibrahim/restaurant/service/impl/FilesSystemStorageService.java
@@ -0,0 +1,88 @@
+package com.ibrahim.restaurant.service.impl;
+
+import com.ibrahim.restaurant.exception.StorageException;
+import com.ibrahim.restaurant.service.StorageService;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Optional;
+
+@Slf4j
+@Service
+public class FilesSystemStorageService implements StorageService {
+
+    @Value("${app.storage.location:uploads}")
+    private String storageLocation;
+
+    private Path rootLocation;
+
+    @PostConstruct
+    public void init() {
+        rootLocation = Paths.get(storageLocation);
+        try {
+            Files.createDirectories(rootLocation);
+        } catch (IOException e) {
+            throw new StorageException("Could not initialize storage exception", e);
+        }
+    }
+
+    @Override
+    public String store(MultipartFile file, String fileName) {
+        try {
+            if (file.isEmpty()) {
+                throw new StorageException("Cannot save empty file");
+            }
+
+            String extension = StringUtils.getFilenameExtension(file.getOriginalFilename());
+            String finalFileName = fileName + "." + extension;
+
+            Path destinationFile = rootLocation
+                    .resolve(Paths.get(fileName))
+                    .normalize()
+                    .toAbsolutePath();
+
+            if (!destinationFile.getParent().equals(rootLocation.toAbsolutePath())) {
+                throw new StorageException("Cannot store file outside specified directory");
+            }
+
+            try (InputStream inputStream = file.getInputStream()) {
+                Files.copy(inputStream, destinationFile, StandardCopyOption.REPLACE_EXISTING);
+            }
+
+            return finalFileName;
+        } catch (Exception e) {
+            throw new StorageException("Failed to store file", e);
+        }
+    }
+
+    @Override
+    public Optional<Resource> loadAsResource(String fileName) {
+        try {
+            Path file = rootLocation.resolve(fileName);
+
+            Resource resource = new UrlResource(file.toUri());
+
+            if (resource.exists() || resource.isReadable()) {
+                return Optional.of(resource);
+            } else {
+                return Optional.empty();
+            }
+        } catch (MalformedURLException e) {
+            log.warn("Could not read file: %s".formatted(fileName), e);
+            return Optional.empty();
+        }
+    }
+}
