bfb14b9ec6ec25dafb13bc3f961d451141146d7e
ibrahimqarayev <qarayevibrahim7@mail.ru>
2026-02-20 18:38:23 +0400

    Add RestaurantService

---
+++ src/main/java/com/ibrahim/restaurant/service/RestaurantService.java
@@ -0,0 +1,26 @@
+package com.ibrahim.restaurant.service;
+
+import com.ibrahim.restaurant.api.restaurant.RestaurantCreateRequest;
+import com.ibrahim.restaurant.api.restaurant.RestaurantUpdateRequest;
+import com.ibrahim.restaurant.entity.Restaurant;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface RestaurantService {
+    Restaurant createRestaurant(RestaurantCreateRequest request);
+
+    Page<Restaurant> searchRestaurants(
+            String query,
+            Float minRating,
+            Float latitude,
+            Float longitude,
+            Float radius,
+            Pageable pageable
+    );
+
+    Restaurant getRestaurant(String id);
+
+    Restaurant updateRestaurant(String id, RestaurantUpdateRequest restaurantUpdateRequest);
+
+    void deleteRestaurant(String id);
+}
