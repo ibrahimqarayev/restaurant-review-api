0f636057d89c27ff50f4e4fc1885f6bf0c4e7772
ibrahimqarayev <qarayevibrahim7@mail.ru>
2026-03-04 16:57:39 +0400

    Add ReviewNotAllowedException

---
+++ src/main/java/com/ibrahim/restaurant/exception/ReviewNotAllowedException.java
@@ -0,0 +1,20 @@
+package com.ibrahim.restaurant.exception;
+
+public class ReviewNotAllowedException extends BaseException {
+
+    public ReviewNotAllowedException() {
+        super();
+    }
+
+    public ReviewNotAllowedException(String message) {
+        super(message);
+    }
+
+    public ReviewNotAllowedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ReviewNotAllowedException(Throwable cause) {
+        super(cause);
+    }
+}
